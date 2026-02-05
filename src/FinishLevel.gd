307fc2b83880635f38773ecb1ce0c8dd4ff37c26
Thomas Burfin <thomasburfinp@gmail.com>
2026-02-05 13:58:41 +0100

    finish line

--- src/FinishLevel.gd
+++ src/FinishLevel.gd
@@ -1,7 +1,9 @@
 extends Node
 
+signal player_hit_finish()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterMovement :
 		var character_movement = body as CharacterMovement
-		if character_movement
+		if character_movement.type == CharacterMovement.TypeEnum.PLAYER :
+			player_hit_finish.emit()
