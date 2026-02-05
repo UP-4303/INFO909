26f5b9a72f642b3a378705daa616b0d877dd8964
Thomas Burfin <thomasburfinp@gmail.com>
2026-02-05 10:36:50 +0100

    ajout module movement et controller

---
+++ src/CharacterMovement.gd
@@ -0,0 +1,71 @@
+class_name CharacterMovement
+extends CharacterBody2D
+
+
+@export
+var _speed = 300.0
+
+@export
+var _jump_velocity = 400.0
+
+@export
+var _affected_by_gravity = true
+
+@export
+var _gravity_mult_coef = 2
+
+var _direction = Vector2.ZERO
+var _jump = false
+
+
+func move_left() -> void :
+	_direction.x = -1
+
+func move_right() -> void :
+	_direction.x = 1
+	
+func move_bottom() -> void :
+	_direction.y = -1
+	
+func move_top() -> void :
+	_direction.y = 1
+	
+func stop() -> void :
+	_direction = Vector2.ZERO
+
+func jump() -> void :
+	_jump = true
+
+
+func _movement_with_gravity(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta * _gravity_mult_coef
+
+	if is_on_floor() and _jump:
+		velocity.y = -_jump_velocity * _gravity_mult_coef
+	
+	if _direction.x != 0:
+		velocity.x = _direction.x * _speed
+	else :
+		velocity.x = move_toward(velocity.x, 0, _speed);
+
+func _movement_without_gravity(delta: float) -> void:
+	_direction = _direction.normalized()
+	if _direction != Vector2.ZERO:
+		velocity = _direction * _speed
+	else :
+		velocity = velocity.move_toward(Vector2.ZERO, _speed)
+		
+	
+
+func _physics_process(delta: float) -> void:
+
+	if _affected_by_gravity:
+		_movement_with_gravity(delta)
+	else:
+		_movement_without_gravity(delta)
+
+	_direction = Vector2.ZERO
+	_jump = false
+
+	move_and_slide()
---
+++ src/controller/PlayerController.gd
@@ -0,0 +1,23 @@
+
+class_name PlayerController
+extends Node
+
+
+@export
+var _characterMovement: CharacterMovement
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.get_action_raw_strength("jump") :
+		_characterMovement.jump()
+		
+	if Input.get_action_raw_strength("left") :
+		_characterMovement.move_left()
+		
+	if Input.get_action_raw_strength("right") :
+		_characterMovement.move_right()
