239dc0f90f2c429247a5a75a6866a1ce2c064274
Djapamal <al.leonarduzzi@gmail.com>
2026-02-05 14:58:33 +0100

    fix erreur barre hp

--- src/UI/health_bar.gd
+++ src/UI/health_bar.gd
@@ -4,16 +4,13 @@ extends HBoxContainer
 @export var HP_indicator_prefab : PackedScene
 @export var health : Health
 
-
 func init_bar(nb_HP : int) -> void:
 	if HP_indicator_prefab == null:
-		printerr("ERREUR : HP_indicator_prefab is not assigned !")
+		printerr("ERROR : HP_indicator_prefab is not assigned !")
 		return
-
 	for i in range(nb_HP):
 		add_heart()
-		
-		
+
 func add_heart() -> void:
 	var new_indicator = HP_indicator_prefab.instantiate()
 	print("create")
@@ -27,18 +24,17 @@ func remove_heart() -> void:
 		remove_child(last_child) 
 
 func update_health(target_HP : int) -> void:
-	target_HP = clampi(target_HP, 0, health.max_health)
-	
+	target_HP = clampi(target_HP, 0, health.max_health)	
 	var current_child_count = get_child_count()
-	
 	while current_child_count > target_HP:
 		remove_heart()
 		current_child_count -= 1
-		
 	while current_child_count < target_HP:
 		add_heart()
 		current_child_count += 1
 
-
 func _ready() -> void:
+	if (not health):
+		printerr("ERROR : Health is not assigned on hp_bar")
+		return
 	init_bar(health.max_health)
